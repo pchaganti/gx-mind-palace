fe9517c1480e0b5dbe254d181a354f4d3aff206d
irvin sachdeva <149496910+1rvinn@users.noreply.github.com>
2025-03-25 21:50:46 +0000

    changed main.py

--- main.py
+++ main.py
@@ -23,7 +23,7 @@ if "init" not in st.session_state:
     st.session_state.tab_selection = "Mindmap"  # Initialize tab selection
 
 # Add this cache decorator for the generate function
-@st.cache_data(show_spinner=False, persist=True)
+@st.cache_data(show_spinner=False, persist="disk")
 def cached_generate(topic_data):
     return generate(topic_data)
 
