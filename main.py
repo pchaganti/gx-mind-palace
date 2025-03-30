ebe8d03b3f0a22eb481e747206ce8f7085134650
irvin sachdeva <149496910+1rvinn@users.noreply.github.com>
2025-03-30 22:47:10 +0530

    improved error handling

--- main.py
+++ main.py
@@ -49,7 +49,6 @@ def process_pdf():
                 st.error("No text could be extracted from the PDF.")
     else:
         st.error("invalid pdf. please enter a valid pdf.")
-        break
     return False
 
 def on_generate():
