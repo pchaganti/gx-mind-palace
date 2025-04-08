4136aade166e3c540cbefe71b4cc0ebaac030656
irvin sachdeva <149496910+1rvinn@users.noreply.github.com>
2025-04-08 23:41:50 +0530

    added save as image functionality

--- generator.py
+++ generator.py
@@ -178,8 +178,8 @@ def generate(topic_data):
                         mermaid_code=generate_mermaid_code_pipeline(relationships)
                     else:
                         mermaid_code=generate_mermaid_code(relationships)
+                    image=mermaid_to_png(mermaid_code)
                     stmd.st_mermaid(mermaid_code)
-                image=mermaid_to_png(mermaid_code)
             st.download_button(label="save as image", data=image,file_name="mindpalace_diagram.png", mime="image/png", key=i) # for saving image
             st.divider()
             i+=1
